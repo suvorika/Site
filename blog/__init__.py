70e64394c8b316a84dd9b53de56f3a4c5186d750
suvorika <iloveirokezzz@gmail.ru>
2023-06-06 16:26:53 +0300

    is vorked

---
+++ blog/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+migrate = Migrate()
+
+login_manager = LoginManager()
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
+login_manager.login_message = 'Авторизуйтесь, что бы попасть на эту страницу!'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile("settings.py")
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
+    from blog.main.routes import main
+    from blog.user.routes import users
+
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+
+    return app
---
+++ blog/user/routes.py
@@ -0,0 +1,78 @@
+import os
+import shutil
+from datetime import datetime
+
+from flask import Blueprint, render_template, flash, request, url_for
+from flask_login import current_user, logout_user, login_required, login_user
+from werkzeug.utils import redirect
+
+from blog import bcrypt, db
+from blog.models import User
+from blog.user.forms import LoginForm, RegistrationForm
+
+users = Blueprint("users", __name__)
+
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.blog"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+
+        full_path = os.path.join(
+            os.getcwd(), "blog/static", "profile_pics", user.username
+        )
+        if not os.path.exists(full_path):
+            os.mkdir(full_path)
+        shutil.copy(f"{os.getcwd()}/blog/static/profile_pics/default.jpg", full_path)
+        flash("Ваш аккаунт был создан. Вы можете войти на блог", "success")
+        return redirect(url_for("users.login"))
+    return render_template(
+        "register.html", form=form, title="Регистрация", legend="Регистрация"
+    )
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.blog"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            flash("Вы вошли как пользователь {current_user.username}", "info")
+            return (
+                redirect(next_page) if next_page else redirect(url_for("users.account"))
+            )
+        else:
+            flash(
+                "Войти не удалось. Пожалуйста, првоерьте электронную почту или пароль",
+                "danger"
+            )
+    return render_template("login.html", form=form, title="Логин", legend="Войти")
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    return render_template("account.html")
+
+
+@users.route("/logout")
+@login_required
+def logout():
+    current_user.last_seen = datetime.now()
+    db.session.commit()
+    logout_user()
+    return redirect(url_for("main.home"))
